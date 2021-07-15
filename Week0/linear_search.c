0fc3ad33a82848e79d767b5f8acfa1a192e91875
prabhatmalhan <prabhatmalhan.cse@geu.ac.in>
2021-07-15 11:54:39 +0530

    main added

--- Week0/linear_search.c
+++ Week0/linear_search.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int linearSearch(int arr[]int l,int key){
+int linearSearch(int arr[],int l,int key){
     int no_of_comparison=0,isFound=0;
     for(int i=0;i<l;++i){
         no_of_comparison++;
@@ -9,6 +9,25 @@ int linearSearch(int arr[]int l,int key){
             break;
         }
     }
-    printf("No. of comparisons : %d",no_of_comparison);
+    printf("No. of comparisons : %d\n",no_of_comparison);
     return isFound;
+}
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    for(int i=0 ; i<n ; ++i){
+        int l;
+        scanf("%d",&l);
+        int arr[l];
+        for(int i=0 ; i<l ; ++i)
+            scanf("%d",&arr[i]);
+        int k;
+        scanf("%d",&k);
+        if(linearSearch(arr,l,k))
+            printf("Found");
+        else
+            printf("Not Found");
+        printf("\n");
+    }
 }
