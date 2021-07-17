4feaa8c3f9603870e99c4501ba786ad65dfa4773
prabhatmalhan <prabhatmalhan.cse@geu.ac.in>
2021-07-17 06:54:26 +0530

    "no test case senario changed"

--- Week0/linear_search.c
+++ Week0/linear_search.c
@@ -1,33 +1,23 @@
 #include<stdio.h>
 
 int linearSearch(int arr[],int l,int key){
-    int no_of_comparison=0,isFound=0;
-    for(int i=0;i<l;++i){
-        no_of_comparison++;
-        if(arr[i]==key){
-            isFound=1;
-            break;
-        }
-    }
-    printf("No. of comparisons : %d\n",no_of_comparison);
-    return isFound;
+    for(int i=0;i<l;++i)
+        if(arr[i]==key)
+            return 1;
+    return 0;
 }
 
 int main(){
-    int n;
-    scanf("%d",&n);
-    for(int i=0 ; i<n ; ++i){
-        int l;
-        scanf("%d",&l);
-        int arr[l];
-        for(int i=0 ; i<l ; ++i)
-            scanf("%d",&arr[i]);
-        int k;
-        scanf("%d",&k);
-        if(linearSearch(arr,l,k))
-            printf("Found");
-        else
-            printf("Not Found");
-        printf("\n");
-    }
+    int l;
+    scanf("%d",&l);
+    int arr[l];
+    for(int i=0 ; i<l ; ++i)
+        scanf("%d",&arr[i]);
+    int k;
+    scanf("%d",&k);
+    if(linearSearch(arr,l,k))
+        printf("Found");
+    else
+        printf("Not Found");
+    printf("\n");
 }
